0a3550b7473d5d72836c68b157d047c377b788b4
petermrecords <email": null>
2017-03-29 15:50:26 -0400

    create routes for comments and reviews

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+  def create
+    if current_user
+      @commentable = find_commentable
+      @comment = @commentable.comments.build(comment_params)
+      @comment.user = current_user
+      @comment.save
+      #error handling?
+      redirect_to :back
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
+  def find_commentable
+    params.each do |name, value|
+      if name =~ /(.+)_id$/
+        return $1.classify.constantize.find(value)
+      end
+    end
+    nil
+  end
+end
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,19 @@
+class ReviewsController < ApplicationController
+  def create
+    if current_user
+      @movie = Movie.find_by(id: params[:movie_id])
+      @review = @movie.reviews.build(review_params)
+      @review.user = current_user
+      @review.save
+      #error handling?
+      redirect_to :back
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:rating, :text)
+  end
+end
