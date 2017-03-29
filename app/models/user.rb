12b76ec585084402b053d86ca7b10b3a60341c2f
petermrecords <email": null>
2017-03-29 11:44:32 -0400

    missed change

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
   has_many :reviews
   has_many :comments
-  has_many :favorites
-  has_many :watchlist
-  has_many :favorite_movies, through: :favorites, source: :movies
-  has_many :watchlist_movies, through: :watchlists, source: :movies
+  has_many :favorite_movies, through: :favorites, source: :movie
+  has_many :watchlist_movies, through: :watchlists, source: :movie
 
   validates :user, :email
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  private
+
+  has_many :favorites
+  has_many :watchlist
 end
