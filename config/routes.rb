4777120055429b715a181d5b34c7a617fbaa4a80
La-Keisha Towner <email": null>
2017-03-29 13:54:23 -0400

    user routes in config folder

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
 
   get 'movies/show'
 
-  get '/users/show'
 
   devise_for :users, controllers: { sessions: 'users/sessions' }
+  resources :users, only: [:show]
 
   root to: 'movies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
