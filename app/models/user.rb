274fc5227f0a6656058ab282e024d06c52620605
Jamie Rogers <james.p.rogers@gmail.com>
2017-03-29 11:09:52 -0400

    user relationships and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
-  
+  has_many :reviews
+  has_many :comments
+  has_many :favorites
+  has_many :watchlist
+  has_many :favorite_movies, through: :favorites, source: :movies
+  has_many :watchlist_movies, through: :watchlists, source: :movies
+
+  validates :user, :email
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
