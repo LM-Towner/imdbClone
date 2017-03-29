62bc46dd950b0b7de55e2d01c527897ddc50f424
petermrecords <email": null>
2017-03-29 11:42:57 -0400

    seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'net/http'
+require 'json'
+
+OMDB_BASEURL = 'http://www.omdbapi.com/?apikey=1394a166?'
+
+['tt3748528', 'tt0076759', 'tt0080684', 'tt0086190', 'tt0120915', 'tt0121765', 'tt0121766', 'tt2488496'].each do |id|
+  uri = (OMDB_BASEURL + "i=#{id}")
+  response = Net::HTTP.get(uri)
+  movie = JSON.parse(response)
+
+  Movie.create(title: movie[:Title],
+               genre: movie[:Genre],
+               release_date: DateTime.parse(movie[:Released]),
+               plot_summary: movie[:Plot],
+               production: movie[:Production],
+               poster: movie[:Poster],
+               website: movie[:Website])
+end
