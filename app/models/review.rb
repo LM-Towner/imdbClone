40351893186c279134e4b04de32f503d50bdb12b
petermrecords <email": null>
2017-03-30 10:47:56 -0400

    validates 1 review per user per movie

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   validates :text, :movie, :user, { presence: true }
   validates :rating, { presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 4 } }
+  validates_uniqueness_of :user_id, { scope: [:movie_id] }
   belongs_to :movie
   belongs_to :user
   has_many :comments, as: :commentable
