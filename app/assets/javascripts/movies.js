82695fc18f909f7222df0cba7ea63f5d7d456c75
Jamie Rogers <james.p.rogers@gmail.com>
2017-03-30 16:31:44 -0400

    adding ajax to reviews

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready ->
+
+  $("#new-review").on("ajax:success", (e, data, status, xhr) ->
+    $("#new-review").append xhr.responseText
+  ).on ("ajax:error", (e, xhr, status, error) ->
+    $("#new-review").append "<p>ERROR</p>"
