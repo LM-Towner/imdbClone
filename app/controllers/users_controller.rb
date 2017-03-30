1375ad66d3e206179e2116c036003c68acfea277
La-Keisha Towner <email": null>
2017-03-30 17:25:58 -0400

    route protection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
+   before_action :authenticate_user!
   def show
+    if current_user
     @user = User.find_by(id: params[:id])
+  else
+    redirect_to new_user_session_path
+    end
   end
 end
