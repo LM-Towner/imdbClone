0310c511da83b02f07e3bd0446976d1f14f77775
La-Keisha Towner <email": null>
2017-03-30 18:02:59 -0400

    user protection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-   before_action :authenticate_user!
   def show
-    if current_user
+    if current_user 
     @user = User.find_by(id: params[:id])
   else
     redirect_to new_user_session_path
